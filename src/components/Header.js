b3a73b25f8653d2cd7fc10560ec020cbacb2ff48
Amr Shakour <amrshakour97@gmail.com>
2022-02-24 19:12:05 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Image from "next/image";
+import {
+    MenuIcon, 
+    SearchIcon, 
+    ShoppingCartIcon,
+} from "@heroicons/react/outline"
+function Header() {
+  return (
+    <header>
+        <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+            <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+                <Image 
+                    src='https://links.papareact.com/f90'
+                    width={150}
+                    height={40}
+                    objectFit="contain"
+                    className='cursor-pointer'
+                />
+            </div>
+
+            {/* custom search element */}
+            <div className='
+            hidden sm:flex items-center flex-grow cursor-pointer
+            rounded-md h-10
+            bg-yellow-400 
+            hover:bg-yellow-500
+            '>
+                <input type="text" className='
+                p-2 h-full 
+                w-6 flex-grow flex-shrink rounded-l-md 
+                focus:outline-none
+                '/>
+                <SearchIcon className='h-12 p-4'/>
+            </div>
+            {/* right */}
+            <div className='
+            text-white flex items-center
+            text-xs space-x-6 mx-6 whitespace-nowrap'>
+                <div className='link'>
+                    <p>Hello Amr </p>
+                    <p className='
+                    font-bold
+                    md:text-sm
+                     '>Account & Lists </p>
+                </div>
+                <div className='link '>
+                    <p>Returns</p>
+                    <p className='
+                    font-bold
+                    md:text-sm
+                     '>& Orders</p>
+                </div>
+                <div className='relative link flex items-center'>
+                    <span className='
+                    absolute top-0 
+                    right-0 md:right-10 h-4 w-4 bg-yellow-400
+                    text-center rounded-full text-black font-bold
+                    '>4</span>
+                    <ShoppingCartIcon className='h-10'/>
+                    <p className='
+                    hidden md:inline mt-2
+                    font-extrabold
+                    md:text-sm
+                     '>Basket</p>
+                </div>
+            </div>
+        </div>
+        {/* bottom half */}
+        <div>
+            <p>
+                <MenuIcon className='h-6 mr-1' />
+            </p>
+        </div>
+    </header>
+  )
+}
+
+export default Header
